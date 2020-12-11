ac4be00140e9ea2f8c8505d9adb208cda304a876
micahmosley <micah.mosley@utexas.edu>
2020-12-10 23:46:11 -0600

    clothing item frequency problem added

---
+++ problems/strings/clothingItems.js
@@ -0,0 +1,5 @@
+//Return the name of the item that shows up the most in the input array
+//If clothing items show up the same amount of time return the item that comes first alphabetically
+
+//input = [greenPants, yellowShirt, redHat, redHat, yellowShirt] -> redHat
+//input= [greenPants, yellowShirt, redHat, redHat, yellowShirt, yellowShirt] -> yellowShirt
