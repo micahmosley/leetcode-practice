082fc45e63cc8e230a7efcb38445592abe0b2041
micahmosley <micah.mosley@utexas.edu>
2020-12-12 18:08:51 -0600

    solution to clothingItems string problem

--- problems/strings/clothingItems.js
+++ problems/strings/clothingItems.js
@@ -3,3 +3,30 @@
 
 //input = [greenPants, yellowShirt, redHat, redHat, yellowShirt] -> redHat
 //input= [greenPants, yellowShirt, redHat, redHat, yellowShirt, yellowShirt] -> yellowShirt
+
+
+countClothes=(clotheArray)=>{
+    const clotheHash={};
+    let max=0;
+    let mostFrequentItems=[];
+
+    for (let i=0; i<clotheArray.length; i++){
+        if (clotheHash[clotheArray[i]]){
+            clotheHash[clotheArray[i]]++
+        } 
+        else clotheHash[clotheArray[i]]=1
+
+        if (clotheHash[clotheArray[i]]>max) {
+            mostFrequentItems=[clotheArray[i]]
+            max=clotheHash[clotheArray[i]]
+        } else if (clotheHash[clotheArray[i]]===max) mostFrequentItems.push(clotheArray[i])
+    }
+
+    mostFrequentItems.sort()
+    return mostFrequentItems[0]
+}
+
+const array=['greenPants', "yellowShirt", "redHat", "redHat", "yellowShirt", "yellowShirt"];
+const array2=["greenPants", "yellowShirt", "redHat", "redHat", "yellowShirt"]
+console.log(countClothes(array))
+console.log(countClothes(array2))
